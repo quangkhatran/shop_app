3947dd06597a1a2da6f44b1c311982be4a05d525
quangkhatran <quangkhatran@gmail.com>
2022-06-22 11:30:34 +0700

    allow Deletion of Item in the shopping cart by swiping left

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
-import '../widgets/cart_item.dart' as ci;
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -51,8 +51,9 @@ class CartScreen extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               itemCount: cart.itemCount,
-              itemBuilder: (ctx, i) => ci.CartItem(
+              itemBuilder: (ctx, i) => CartItem(
                 cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
                 cart.items.values.toList()[i].price,
                 cart.items.values.toList()[i].quantity,
                 cart.items.values.toList()[i].title,
