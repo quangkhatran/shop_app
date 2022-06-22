aab574c04753eeb03e79ba560b67b716c5b06d27
quangkhatran <quangkhatran@gmail.com>
2022-06-22 11:05:24 +0700

    display list of items and the total sum of money in the shopping cart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import './cart_screen.dart';
 
 enum FilterOptions {
   Favorites,
@@ -57,7 +59,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               icon: Icon(
                 Icons.shopping_cart,
               ),
-              onPressed: () {},
+              onPressed: (){
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+                },
             ),
           ),
         ],
