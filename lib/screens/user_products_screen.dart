547e09ea412882b8c34553e116077a8af31dd620
quangkhatran <quangkhatran@gmail.com>
2022-06-23 14:26:24 +0700

    Add "New Product" form

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/widgets/app_drawer.dart';
-
 import 'package:provider/provider.dart';
+
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -18,7 +18,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
