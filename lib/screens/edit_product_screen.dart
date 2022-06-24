6eb58d263a6387c8ce2aa13915c19236169b700a
quangkhatran <quangkhatran@gmail.com>
2022-06-24 23:55:00 +0700

    Update and delete data using Patch and Delete Http Request

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -108,11 +108,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
     if (_editedProduct.id != null) {
       Provider.of<Products>(context, listen: false)
-          .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
+          .updateProduct(_editedProduct.id, _editedProduct)
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+        Navigator.of(context).pop();
       });
-      Navigator.of(context).pop();
+      
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
