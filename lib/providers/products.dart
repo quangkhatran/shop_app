397ffcc2df1d864773d4aca2d67811ab918d63f3
quangkhatran <quangkhatran@gmail.com>
2022-06-24 18:21:36 +0700

    try "async" and "await"

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,23 +67,22 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     final url = Uri.parse(
         'https://flutter-3074f-default-rtdb.asia-southeast1.firebasedatabase.app/products.json');
-    return http
-        .post(
-      url,
-      body: json.encode(
-        {
-          'title': product.title,
-          'description': product.description,
-          'imageUrl': product.imageUrl,
-          'price': product.price,
-          'isFavorite': product.isFavorite,
-        },
-      ),
-    )
-        .then((response) {
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'isFavorite': product.isFavorite,
+          },
+        ),
+      );
       print(json.decode(response.body));
       final newProduct = Product(
         title: product.title,
@@ -95,10 +94,10 @@ class Products with ChangeNotifier {
       );
       _items.add(newProduct);
       notifyListeners();
-    }).catchError((error){
+    } catch (error) {
       print(error);
-      throw error;
-    });
+      throw (error);
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
