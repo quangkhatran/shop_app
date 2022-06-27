124aaa4a13caae9fe8482bf1f94e869fe5ed5463
quangkhatran <quangkhatran@gmail.com>
2022-06-28 00:56:57 +0700

    add User Login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,9 +8,9 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBmEBPlGg6lZowJeolvFFjME8RJvZFbtAY');
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:${urlSegment}?key=AIzaSyBmEBPlGg6lZowJeolvFFjME8RJvZFbtAY');
     final response = await http.post(
       url,
       body: json.encode({
@@ -19,5 +20,14 @@ class Auth with ChangeNotifier {
       }),
     );
     print(json.decode(response.body));
+  }
+
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+   return _authenticate(email, password, 'signInWithPassword');
   }
 }
